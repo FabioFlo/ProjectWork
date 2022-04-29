b6d482d96b2730a5517f65bf64d4f7acd9ef9590
EbekSynthsei <emanuele.s.tagliamonte@gmail.com>
2022-04-29 02:52:41 +0200

    VERSIONE CON SEARCH FUNZIONANTE
    Sono fiero di annunciare che il nostro metodo di search è qui
    
    Co-Authored-By: macco14 <104018755+macco14@users.noreply.github.com>

--- src/main/java/com/g3/projectwork/controller/SearchController.java
+++ src/main/java/com/g3/projectwork/controller/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.g3.projectwork.entities.Gioco;
 import com.g3.projectwork.repos.GiocoRepository;
@@ -17,18 +18,20 @@ public class SearchController {
 	@Autowired
 	GiocoRepository giocoRepository;
 
-	@GetMapping("/search/{word}")
-	public String searchByWord(@PathVariable("word") String word, Model model)
+	@GetMapping("/search")
+	public String searchByWord(@RequestParam(value="word") String word, Model model)
 	{
 		String redirect = "";
 		//ESCLUDO PAROLE NULLE
 		if(word==null || word.isBlank() || word.isEmpty()) {
-			redirect = "/giochi/listGioco";
+			System.out.println("PAROLA NULLA O VUOTA");
+			redirect = "redirect:/giochi/listGioco";
 		}else {
 			//SE HO PAROLE CONTROLLO I GIOCHI
 			List<Gioco> giochi = giocoRepository.findByTitoloContaining(word);
 			if(giochi.isEmpty() || giochi == null) {
-				redirect = "/giochi/listGioco";
+				System.out.println("LISTA NULLA O VUOTA");
+				redirect = "redirect:/giochi/listGioco";
 			} //SE HO I GIOCHI LI ASSEGNO AL MODEL E RICARICO ADMINGIOCOPAGE MA PASSANDOGLI UN MODELLO
 			else {
 				model.addAttribute("giochi", giochi);
